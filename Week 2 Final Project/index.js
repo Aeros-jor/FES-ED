3df0b127d29b54cbb0e7df98ea04ac67e4310d92
Aeros-jor <edpritchard.dev@gmail.com>
2024-08-09 11:31:44 -0500

    Final project Testimonial

---
+++ Week 2 Final Project/index.js
@@ -0,0 +1,9 @@
+function leftClick() {
+  document.getElementById("left__button").style.border = "solid";
+  document.getElementById("left__button").style.borderWidth = "2px";
+  document.getElementById("left__button").style.borderColor = "lightblue";
+}
+function rightClick() {
+  document.getElementById("left__button").style.border = "solid";
+  document.getElementById("right__button").style.borderColor = "lightblue";
+}
