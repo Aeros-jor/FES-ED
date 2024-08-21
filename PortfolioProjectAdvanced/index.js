21803d7779de5d27dc4783d780f7157a4e4b2ba0
Aeros-jor <edpritchard.dev@gmail.com>
2024-08-21 15:09:46 -0500

    project completed

--- PortfolioProjectAdvanced/index.js
+++ PortfolioProjectAdvanced/index.js
@@ -1,6 +1,32 @@
 // public key = FaS0mwfLDEDkby8yG
 // serviceID = service_me8zm8y
 // templateID = template_y2ic17m
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+    
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+    
+}
 
 function contact(event) {
     event.preventDefault();
@@ -25,7 +51,6 @@ function contact(event) {
         })
 }
 
-let isModalOpen = false;
 function toggleModal() {
     if (isModalOpen) {
         isModalOpen = !isModalOpen;
