1e1ab18bb366ffaa2fa9a845ad599aa8cf67c33a
Aeros-jor <edpritchard.dev@gmail.com>
2024-08-26 18:25:01 -0500

    update

--- Week3FinalProjectAPIs/index.js
+++ Week3FinalProjectAPIs/index.js
@@ -5,7 +5,13 @@ async function main() {
     const moviesData = await movies.json();
     console.log(moviesData)
     const movieListEl = document.querySelector(".movie-list")
-    movieListEl.innerHTML = moviesData.map((movie) => movieHTML(movie)).join();
+    if (moviesData.Search && Array.isArray(moviesData.Search)) {
+        movieListEl.innerHTML = moviesData.Search.map((movie) =>
+          movieHTML(movie)
+        ).join("");
+      } else {
+        console.error("No movies found or invalid response:", moviesData);
+      }
 
 }
 
@@ -16,9 +22,9 @@ function movieHTML(movie){
             <div class="movie">
               <div class="movie-card">
                 <div class="movie-card__container">
-                  <h1>${movie.strMeal}</h1>
-                    <p><b>Meal Id:${movie.idMeal}</b> </p>
-                    <p><b><img src="${movie.strMealThumb}" alt="" class="movie--img"></b> </p>
+                  <h1>${movie.Title}</h1>
+                    <p><b>Year:${movie.Year}</b> </p>
+                    <p><b><img src="${movie.Poster}" alt="" class="movie--img"></b> </p>
                 </div>
               </div>
             </div>
